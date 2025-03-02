80abf7b07cd3f4b8f12a4c8a5fca5db14a6f38d3
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-03-02 06:59:08 +0100

    опрацьовую лекцію

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-const express = require('express');
+import {Response, Request} from "express";
+import express from "express";
+import * as mongoose from "mongoose";
+
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 const PORT = 5000;
-
 const JohnDoe = [
     {
         name: 'John Doe 1',
@@ -39,40 +39,26 @@ const JohnDoe = [
         gender: 'Male',
     }
 ]
-
-app.get('/JohnDoe', (req, res) => {
+app.get('/JohnDoe', (req: Request, res: Response) => {
     res.json(JohnDoe)
 })
-
-app.post('/JohnDoe', (req, res) => {
+app.post('/JohnDoe', (req: Request, res: Response) => {
     const johnDoe = req.body;
     JohnDoe.push(johnDoe);
-
     res.status(201).json({message: 'JohnDoe created!'});
 })
-
-app.put('/JohnDoe/:id', (req, res) => {
+app.put('/JohnDoe/:id', (req: Request, res: Response) => {
     const {id} = req.params;
-    JohnDoe[id] = req.body;
-
-    res.status(200).json({
-        message: "JohnDoe updated successfully!",
-        data: JohnDoe[+id],
-    })
+    JohnDoe[+id] = req.body;
+    res.status(200).json({message: "JohnDoe updated successfully!", data: JohnDoe[+id]})
 })
 
-
-app.delete('/JohnDoe/:id', (req, res) => {
+app.delete('/JohnDoe/:id', (req: Request, res: Response) => {
     const {id} = req.params;
-
     JohnDoe.splice(+id, 1);
-
-    res.status(200).json({
-        message: "JohnDoe updated successfully!",
-        data: JohnDoe[+id],
-    })
+    res.status(200).json({message: "JohnDoe updated successfully!", data: JohnDoe[+id]})
 })
-
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    mongoose.connect("mongodb://127.0.0.1:27017/preview");
+    console.log(`Listening on port ${PORT}`)
 });
