eb40bb11764b73ac33c43f108d0299d51dbb0537
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-03-03 03:58:04 +0100

    налаштував Eslint (можливо)

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,26 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-config-prettier');
+const prettierPlugin = require('eslint-plugin-prettier');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
 
-export default tseslint.config(
+module.exports = [
     eslint.configs.recommended,
     tseslint.configs.recommended,
-);
+    prettier,
+    {
+        languageOptions: {
+            parser: tseslint.parser,
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            prettier: prettierPlugin,
+            'simple-import-sort': simpleImportSort,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+        },
+    }
+];
