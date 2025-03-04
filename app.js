d0a6134a5bd0195f2fbe658b7206eee1d2ebd9ec
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-03-04 20:13:32 +0100

    прибрав зайвий код

--- app.js
+++ app.js
@@ -5,26 +5,18 @@ const readline = require('node:readline/promises');
 
 const start = async () => {
     const filePath = path.join('email', 'general', 'emails.txt');
-    const typeOfMail = [];
     await afs.mkdir(path.join('email', 'separated'), {recursive: true});
 
     const fileStream = fs.createReadStream(filePath, 'utf-8');
     const rl = readline.createInterface({input: fileStream});
+
     try {
         for await (const line of rl) {
             const email = line.split(/\s+/)[1];
             const domain = email.split('@')[1];
             const pathDomain = path.join('email', 'separated', `${domain}.txt`);
-
-            if (!typeOfMail.includes(domain)) {
-                typeOfMail.push(domain);
-                await afs.writeFile(pathDomain, `${email}\n`)
-            } else {
-                await afs.appendFile(pathDomain, `${email}\n`);
-            }
-
+            await afs.appendFile(pathDomain, `${email}\n`);
         }
-
     } finally {
         await rl.close();
     }
